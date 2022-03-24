33159a0c0c9a51099b4895c4b07b5659d8bd745a
H0llyFire <79707862+H0llyFire@users.noreply.github.com>
2022-03-24 23:57:58 +0100

    Simple formating adjustments 1/3

--- IlluFormer/code/Graphics/Texture.h
+++ IlluFormer/code/Graphics/Texture.h
@@ -12,16 +12,13 @@ private:
 	std::string m_FilePath;
 	unsigned char* m_LocalBuffer;
 	int m_Width, m_Height, m_BPP;
-public:
 
+public:
 	Texture(const std::string& path);
 	~Texture();
-	//Texture(const Texture& oldTexture); //Copy Init
 
 	void Bind(unsigned int slot = 0) const;
 	void Unbind() const;
-
-	
 
 	inline int GetWidth() const { return m_Width; }
 	inline int GetHeight() const { return m_Height; }
